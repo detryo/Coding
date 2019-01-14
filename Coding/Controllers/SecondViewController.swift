cb21f7ab23953e7d8e466c0ce4b5ecf86bac7ff5
Cristian <chris.sedano.arenas@gmail.com>
2019-01-14 15:22:06 +0000

    UISlider, Fibonacci numbers
    Implement Slider, Label and Button in Main.storyboard, FirstViewController, in FirstViewController.swift, implement some var, IBAction func sliderAgeMoved and validateData, and new func updateAgeLabel, SecondViewController.swift, implement some var and func generateFibonacciNumbers

--- Coding/Controllers/SecondViewController.swift
+++ Coding/Controllers/SecondViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    var fibonacci : [Int] = [0,1]
+    var fibonacciID = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func generateFibonacciNumbers(){
+        
+        if (fibonacciID <= 1 || (fibonacciID >= 200)) {
+            return
+        }
+        // Generar los numeros de Fibonacci hasta el fibonacciID
+        
+    }
 
 }
 
