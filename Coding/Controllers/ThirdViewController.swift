206bdafc461bbb8fce790fcdaafaca26732cb500
Cristian <chris.sedano.arenas@gmail.com>
2019-01-21 16:40:36 +0000

    UITableView & UIFont
    ThirdViewController, implement Table View and table view cell, inside ThirdViewController.swift, implement some car families and fonts, and some func numberOfSections, func tableView and config

--- Coding/Controllers/ThirdViewController.swift
+++ Coding/Controllers/ThirdViewController.swift
@@ -8,23 +8,36 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var families : [String] = []
+    var fonts : [String: [String]] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        families  = UIFont.familyNames.sorted(by: {return $0 < $1})
+        for fam in families {
+            fonts[fam] = UIFont.fontNames(forFamilyName: fam)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Method of Protocol UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.families.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FontFamillyCell", for: indexPath)
+        let fontFamily = families[indexPath.row]
+        cell.textLabel?.text = fontFamily
+        cell.textLabel?.font = UIFont(name: fontFamily, size: 20.0)
+        return cell
+    }
 }
