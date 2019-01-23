25431651c20053149126ca1181cb9877abebb2b5
Cristian <chris.sedano.arenas@gmail.com>
2019-01-23 16:53:23 +0000

    UIPickerView
    implement UIPIckerViewDelegate and UIPickerViewDataSource, and some func and config

--- Coding/Controllers/FontDetailViewController.swift
+++ Coding/Controllers/FontDetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FontDetailViewController: UIViewController {
+class FontDetailViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     var familiName : String = ""
     var fonts : [String] = []
@@ -21,12 +21,38 @@ class FontDetailViewController: UIViewController {
         super.viewDidLoad()
         
         self.labelTitle.text = familiName
-        self.labelTitle.font = UIFont(name: familiName, size: 16.0)
+        self.labelTitle.font = UIFont(name: familiName, size: 22.0)
+        
+        if fonts.count == 0 {
+            self.pickerFonts.isHidden = true
+        }
+        
+        self.pickerFonts.dataSource = self
+        self.pickerFonts.delegate = self
+        self.textView.font = UIFont(name: familiName, size: 15.0)
 
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         
         self.dismiss(animated: true)
+    }
+    // MARK: - UIPIckerView Data Source Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.fonts.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fonts[row]
+    }
+    // MARK: - UIPickerView Delegate
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let font = fonts[row]
+        self.textView.font = UIFont.init(name: font, size: 15.0)
     }
 }
